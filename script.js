8fe642740c9ad7ff1a0ec1133c04eb85bb447b93
Kainã de Melo Ramos <kaina.ramos@outlook.com>
2024-01-31 17:09:45 -0400

    Adicionado um script em javascript para display do resultado de busca

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+/* Define constantes que carregam em si os elementos da página que possuem o ID
+especificado */
 const searchInput = document.getElementById('search-input');
-const resutlsArtists = document.getEelementById('result-artist');
+const resultArtists = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('discover');
 
+// Busca na API o termo digitado pelo usuário
 function requestApi(searchTerm) {
-/* Tentar iniciar o json-server em casa*/
-    const url = `http://localhost:3000/artists?name_like=$(searchTerm)`
+// Tentar iniciar o json-server em casa*/
+    const url = `http://http://127.0.0.1:5500/progresso%20aula%2004/artists?name_like=$(searchTerm)`
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
 }       
+// Exibição dos resultados de pesquisa
+function displayResults(result) {
+    // Oculta a exibição de playlists
+    resultPlaylist.classList.add(`hidden`);
+    const artistName = document.getElementById(`artist-name`);
+    const artistImage = document.getElementById(`artist-img`);
 
+    /* Atribui o nome e a imagem carregadas na API às constantes que referenciam os 
+    elementos de artistas na página */
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    // Remove a classe hidden do elemento carregado em resultArtists
+    resultArtists.classList.remove(`hidden`);
+} 
 
 document.addEventListener('input', function() {
     const searchTerm = searchInput.value.toLowerCase
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden')
-        resutlsArtists.classList.remove('hidden');
+        resultArtists.classList.remove('hidden');
         return;
     }
 
